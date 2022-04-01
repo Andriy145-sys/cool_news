8cb94122846b727b92cb443c6daa3e87c759beeb
Andriy145-sys <77618488+Andriy145-sys@users.noreply.github.com>
2022-04-01 20:19:34 +0300

    added pagination in main-component

--- src/components/Main/main-component.vue
+++ src/components/Main/main-component.vue
@@ -1,7 +1,8 @@
 <template>
-  <v-row style="justify: space-between">
-    <posts-list :posts="posts"/>
-  </v-row>
+  <div>
+    <posts-list :posts="posts" />
+    <v-pagination v-model="page" :length="6" circle color="green" />
+  </div>
 </template>
 
 <script>
@@ -11,11 +12,28 @@ export default {
     postsList,
   },
   data: () => ({
+    page: 1,
     posts: [
-      { _id: 1, title: "Cool news", text: "Text dsahdsajdghjsagdhsgadgjsgdjashgdjsagdhsagjd" },
-      { _id: 2, title: "Cool news", text: "TextText dsahdsajdghjsagdhsgadgjsgdjashgdjsagdhsagjd" },
-      { _id: 3, title: "Cool news", text: "TexText dsahdsajdghjsagdhsgadgjsgdjashgdjsagdhsagjdt" },
-      { _id: 4, title: "Cool news", text: "TText dsahdsajdghjsagdhsgadgjsgdjashgdjsagdhsagjdext" },
+      {
+        _id: 1,
+        title: "Cool news",
+        text: "Text dsahdsajdghjsagdhsgadgjsgdjashgdjsagdhsagjd",
+      },
+      {
+        _id: 2,
+        title: "Cool news",
+        text: "TextText dsahdsajdghjsagdhsgadgjsgdjashgdjsagdhsagjd",
+      },
+      {
+        _id: 3,
+        title: "Cool news",
+        text: "TexText dsahdsajdghjsagdhsgadgjsgdjashgdjsagdhsagjdt",
+      },
+      {
+        _id: 4,
+        title: "Cool news",
+        text: "TText dsahdsajdghjsagdhsgadgjsgdjashgdjsagdhsagjdext",
+      },
       { _id: 5, title: "Cool news", text: "Text" },
     ],
   }),
