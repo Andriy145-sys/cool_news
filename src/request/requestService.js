cf8b5f88de855e5147f7e3aacce7ea7024a31212
Andriy145-sys <77618488+Andriy145-sys@users.noreply.github.com>
2022-04-09 20:42:49 +0300

    change api endpoint

--- src/request/requestService.js
+++ src/request/requestService.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_ENDPOINT = 'http://localhost:8000'
+//const API_ENDPOINT = 'http://localhost:8000'
+const API_ENDPOINT = 'https://coolnewsbackend.herokuapp.com/'
 
 export default {
 
